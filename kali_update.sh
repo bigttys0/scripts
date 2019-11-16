6edc32f585fcbeecda652ad49f6aa073d52caa0b
bigttys0 <lokoljokol@gmail.com>
2019-11-15 21:01:55 -0800

    Add files via upload

---
+++ kali_update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+cd
+clear
+
+tput setaf 3; tput bold; echo "[+] Updating Kali. Please wait."
+apt update -y && apt dist-upgrade -y
+
+sleep 1
+
+tput setaf 3; tput bold; echo "[+] Kali is now up to date."
+
+sleep 1
+
+tput setaf 6; tput bold; echo "[+] Cleaning Kali. Please wait."
+apt autoremove -y && apt autoclean -y && apt clean -y
+tput setaf 3; tput bold;
+
+sleep 1 
+
+tput setaf 2; tput bold; echo "[+] Kali is now less dirty!"
+
+sleep 1 
+
+tput setaf 1; tput bold; echo 
+read -p "Process is complete, do you want to reboot now? " -n 1 -r
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    /sbin/reboot
+fi
+
+clear
+cd
